b5db9de91af50f7278f6f7267788bafa2c4292ae
Enrico Raphael Skoruppa <esdrifter@gmail.com>
2026-02-09 12:12:54 +0100

    removed unnecessary imports

--- rbpnfe/midstep_composites.py
+++ rbpnfe/midstep_composites.py
@@ -5,8 +5,8 @@ import numpy as np
 from typing import List, Tuple, Callable, Any, Dict
 
 from .PolyCG.polycg.SO3 import so3
-from .PolyCG.polycg.transforms.transform_SO3 import euler2rotmat_so3
-from .PolyCG.polycg.transforms.transform_marginals import send_to_back_permutation
+# from .PolyCG.polycg.transforms.transform_SO3 import euler2rotmat_so3
+# from .PolyCG.polycg.transforms.transform_marginals import send_to_back_permutation
 
 
 def calculate_midstep_triads(
